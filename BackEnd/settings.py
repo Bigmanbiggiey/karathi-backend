080641fb8adef34be3f7e48c081cdef29a3ca55b
Bigmanbiggiey <ndiranguh02@gmail.com>
2025-11-20 10:04:54 +0300

    Backend updates

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-5s)^awpej@*#^)f_^1tp@a-n$g)2-ejv(!laz1%o7ndc*g2m$7
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "*"
+    ".railway.app",
+    "karathi-frontend-production.up",
 ]
 
 
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,7 +76,9 @@ REST_FRAMEWORK = {
     ),
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://karathi-frontend-production.up.railway.app/",
+]
 CORS_ALLOW_HEADERS = ['*']
 CORS_ALLOW_METHODS = [
     "DELETE",
@@ -83,6 +87,10 @@ CORS_ALLOW_METHODS = [
     "PATCH",
     "POST",
     "PUT",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+   "https://karathi-frontend-production.up.railway.app/",
 ]
 
 ROOT_URLCONF = 'BackEnd.urls'
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn BackEnd.wsgi --log-file -
+web: gunicorn BackEnd.wsgi --bind 0.0.0.0:$PORT --log-file -
